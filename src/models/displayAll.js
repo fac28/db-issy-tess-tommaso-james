2577b89407828dac3eabdb2aff9f5e8bfd2b9204
tess-phillips <tess.phillips@me.com>
2023-09-27 12:50:27 +0100

    select all functions working

--- src/models/displayAll.js
+++ src/models/displayAll.js
@@ -10,20 +10,46 @@ function listVenues() {
     return select_venues.all();
 }
 
-console.log(listVenues())
+const select_locations = db.prepare(/*sql*/ `
+  SELECT
+    name,
+    street,
+    postcode
+  FROM location
+`);
+
+function listLocations() {
+    return select_locations.all();
+}
 
 
-// const select_venue_info = db.prepare(/*sql*/ `
-//   SELECT
-//     venue.name,
-//     location,
-    
-    
-//   FROM products
-// `);
+const select_venue_info = db.prepare(/*sql*/ `
+  SELECT
+    venue.name,
+    location.street
+  FROM venue
+  JOIN location ON venue.location_id = location.id
+`);
 
-// function listVenueInfo() {
-//     return select_venues.all();
-// }
+function listVenueInfo() {
+    return select_venue_info.all();
+}
 
-module.exports = { listVenues };
+const select_venue_cuisines = db.prepare(/*sql*/ `
+    SELECT
+        venue.name AS venue_name,
+        cuisine.name AS cuisine_name
+    FROM venue
+    JOIN venue_cuisine ON venue.id = venue_cuisine.venue_id
+    JOIN cuisine ON venue_cuisine.cuisine_id = cuisine.id
+`);
+
+function listVenueCuisines() {
+    return select_venue_cuisines.all();
+}
+
+
+/*console.log(listVenueCuisines())*/
+
+
+module.exports = { listVenues, listLocations, listVenueInfo, listVenueCuisines };
